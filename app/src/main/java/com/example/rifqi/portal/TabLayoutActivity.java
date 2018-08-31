d73ca21891ec877a3c2bfe64c569a0756d5fd9c2
rifqieizan <rifqi.eizan@gmail.com>
2018-08-31 08:29:12 +0700

    commit

---
+++ app/src/main/java/com/example/rifqi/portal/TabLayoutActivity.java
@@ -0,0 +1,52 @@
+package com.example.rifqi.portal;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Hafizh Herdi on 8/3/2015 www.twoh.co
+ */
+public class TabLayoutActivity extends AppCompatActivity{
+
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_layout);
+
+        // rest of code
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+        tabLayout.addTab(tabLayout.newTab().setText("Profile"));
+        tabLayout.addTab(tabLayout.newTab().setText("Settings"));
+        tabLayout.addTab(tabLayout.newTab().setText("More"));
+        tabLayout.addTab(tabLayout.newTab().setText("About"));
+        tabLayout.addTab(tabLayout.newTab().setText("Help"));
+        tabLayout.addTab(tabLayout.newTab().setText("Friends"));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tab.getPosition();
+                tab.getText();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+    // rest of code
+}
